f17a8a3c389ff13a76d7314cde9a00f86fdac4b9
char-tay <charlieataylor@gmail.com>
2014-10-19 16:24:37 -0400

    added unity

---
+++ unity/Assets/Scripts/BallControl.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+	var randomNumber = Random.Range(0,2); // spits integers
+	if(randomNumber == 1) {
+		rigidbody2D.AddForce(new Vector2 (80,10));
+		Debug.Log("Shoot right " + randomNumber);
+	}
+	else {
+		rigidbody2D.AddForce(new Vector2(-80,-10));
+		Debug.Log("Shoot left " + randomNumber);
+	}
+}
+
+function OnCollisionEnter2D (colInfo : Collision2D) {
+	if (colInfo.collider.tag == "Player") {
+		rigidbody2D.velocity.y = rigidbody2D.velocity.y/2 + colInfo.collider.rigidbody2D.velocity.y/3;
+	}
+}
---
+++ unity/Assets/Scripts/GameManager.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+static var playerScore01 : int = 0;
+static var playerScore02 : int = 0;
+
+var theSkin : GUISkin;
+
+static function Score(wallName : String) {
+	if(wallName == "rightWall") {
+		playerScore01++;	
+	}
+	else {
+		playerScore02++;
+	}
+	
+	Debug.Log("player1 score: " + playerScore01 + " player2 score: " + playerScore02);
+}
+
+function OnGUI() {
+	GUI.skin = theSkin;
+	GUI.Label (new Rect (Screen.width/2 - 150, 20, 100, 100), "" + playerScore01);
+	GUI.Label (new Rect (Screen.width/2 + 150, 20, 100, 100), "" + playerScore02);
+}
